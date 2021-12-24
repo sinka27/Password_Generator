87f70be26a40db831b34e23654d01406c8e0f4de
sinka27 <sinka27392@gmail.com>
2021-12-23 23:16:53 -0800

    1. Creating a function called genPassword 2. Asking user for length of their password until they enter length between 8 to 128
    adding do-while

--- script.js
+++ script.js
@@ -7,6 +7,21 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+//Generate Password according to choosen inputs
+function genPassword() {
+  var passwordLength;
+  var chars = "";
+
+  //prompts password criteria until atleast 1 character type is selected
+  do {
+    //Prompt for chosing the length of the password until correct length is entered
+    do {
+      passwordLength = prompt(
+        "What should be the length of your password? Enter a number between 8-128"
+      );
+    } while (passwordLength < 8 || passwordLength > 128);
+  
+  } while (chars === "");
 
 }
 
